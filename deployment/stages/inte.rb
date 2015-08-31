b743810f2452221e9b38efd7c8d9666fe4ddc8a9
romain boessel <romain.boessel@businessdecision.com>
2015-08-31 11:37:41 +0200

    remove test environment

--- deployment/stages/inte.rb
+++ deployment/stages/inte.rb
@@ -1,4 +1,4 @@
 set :application, 'OpenOrchestraFront'
 
-server 'open_orchestra_front_inte', roles: %w{web app db}
+server 'open_orchestra_front_inte', roles: %w{web app db env}
 set :deploy_to, '/var/www/front-open-orchestra'
--- deployment/stages/inte2.rb
+++ deployment/stages/inte2.rb
@@ -1,4 +1,4 @@
 set :application, 'OpenOrchestraFront2'
 
-server 'open_orchestra_front2_inte', roles: %w{web app db}
+server 'open_orchestra_front2_inte', roles: %w{web app db env}
 set :deploy_to, '/var/www/front-open-orchestra2'
