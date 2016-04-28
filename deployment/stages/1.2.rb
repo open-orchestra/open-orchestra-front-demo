d8b992f5c3438e1aefcce1207bf4d7e95804b6f6
Amaury Lavieille <amaury.lavieille@businessdecision.com>
2016-04-28 18:32:45 +0200

    update provisionning inte and deploy 1.2

--- deployment/stages/1.2.rb
+++ deployment/stages/1.2.rb
@@ -1,6 +1,6 @@
 set :application, 'OpenOrchestraFront'
 
-server 'open_orchestra_front_inte', roles: %w{web app db env}
+server 'open_orchestra_front_inte_1-2', roles: %w{web app db env}
 set :deploy_to, '/var/www/front-open-orchestra'
 set :update_dir, 'update-vendor-front-inte'
 set :git_project_dir, 'open-orchestra-front-demo'
