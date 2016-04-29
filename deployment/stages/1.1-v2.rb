82b93ddeda10c2f2bd74d86938e8937f467a018e
Amaury Lavieille <amaury.lavieille@businessdecision.com>
2016-04-29 11:23:31 +0200

    add host deploy 1.1

--- deployment/stages/1.1-v2.rb
+++ deployment/stages/1.1-v2.rb
@@ -1,6 +1,6 @@
 set :application, 'OpenOrchestraFront2'
 
-server 'open_orchestra_front2_inte', roles: %w{web app db env}
+server 'open_orchestra_front2_inte_1-1', roles: %w{web app db env}
 set :deploy_to, '/var/www/front-open-orchestra2'
 set :update_dir, 'update-vendor-front-inte2'
 set :git_project_dir, 'open-orchestra-front-demo'
