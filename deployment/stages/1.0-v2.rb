8fee89cf7de7cc4eeab2df91aae007a9fabdac9b
Amaury Lavieille <amaury.lavieille@businessdecision.com>
2016-04-29 12:29:23 +0200

    update deploy 1.0

--- deployment/stages/1.0-v2.rb
+++ deployment/stages/1.0-v2.rb
@@ -1,4 +1,4 @@
 set :application, 'OpenOrchestraFront2'
 
-server 'open_orchestra_front2_inte', roles: %w{web app db env}
+server 'open_orchestra_front2_inte_1-0', roles: %w{web app db env}
 set :deploy_to, '/var/www/front-open-orchestra2'
--- deployment/stages/1.0.rb
+++ deployment/stages/1.0.rb
@@ -1,4 +1,4 @@
 set :application, 'OpenOrchestraFront'
 
-server 'open_orchestra_front_inte', roles: %w{web app db env}
+server 'open_orchestra_front_inte_1-0', roles: %w{web app db env}
 set :deploy_to, '/var/www/front-open-orchestra'
