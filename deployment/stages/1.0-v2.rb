8d913e80b6bfe9ab3f655bdc0dbf07bc348ce2cc
Amaury Lavieille <amaury.lavieille@businessdecision.com>
2016-04-29 16:17:44 +0200

    add task composer update capistrano

--- deployment/stages/1.0-v2.rb
+++ deployment/stages/1.0-v2.rb
@@ -2,3 +2,6 @@ set :application, 'OpenOrchestraFront2'
 
 server 'open_orchestra_front2_inte_1-0', roles: %w{web app db env}
 set :deploy_to, '/var/www/front-open-orchestra2'
+set :update_dir, 'update-vendor-front_inte'
+set :git_project_dir, 'open-orchestra-front-demo'
+set :branch, '1.0'
