26338d91b4e2764368f350c4edba10d7f0167573
itkg-ngilain <noel.gilain@businessdecision.com>
2016-07-04 15:44:14 +0200

    Update conf according to sensio/distribution-bundle v5

--- web/app_dev.php
+++ web/app_dev.php
@@ -3,10 +3,8 @@
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Debug\Debug;
 
-$loader = require_once __DIR__.'/../app/bootstrap.php.cache';
+$loader = require_once __DIR__.'/../app/autoload.php';
 Debug::enable();
-
-require_once __DIR__.'/../app/AppKernel.php';
 
 $kernel = new AppKernel('dev', true);
 $kernel->loadClassCache();
