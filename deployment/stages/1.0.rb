85632473b693420d449b5889861ea9e62157516a
Amaury Lavieille <amaury.lavieille@businessdecision.com>
2016-08-03 12:36:46 +0200

    update deploy

--- deployment/stages/1.0.rb
+++ deployment/stages/1.0.rb
@@ -1,7 +1,7 @@
 set :application, 'OpenOrchestraFront'
 
 server 'open_orchestra_front_inte_1-0', roles: %w{web app db env}
-set :deploy_to, '/var/www/front-open-orchestra'
+set :deploy_to, '/home/wwwroot/openorchestra/front'
 set :update_dir, 'update-vendor-front_inte'
 set :git_project_dir, 'open-orchestra-front-demo'
 set :branch, '1.0'
