73cb5b16f95f544a070332b6e283af0905c7ce30
Amaury Lavieille <amaury.lavieille@businessdecision.com>
2016-09-15 14:31:16 +0200

    add list content type car block hello

--- src/FormationBundle/DisplayBlock/HelloBlockDisplayStrategy.php
+++ src/FormationBundle/DisplayBlock/HelloBlockDisplayStrategy.php
@@ -4,10 +4,18 @@ namespace FormationBundle\DisplayBlock;
 
 use OpenOrchestra\DisplayBundle\DisplayBlock\Strategies\AbstractStrategy;
 use OpenOrchestra\ModelInterface\Model\ReadBlockInterface;
+use OpenOrchestra\ModelInterface\Repository\ContentRepositoryInterface;
 
 class HelloBlockDisplayStrategy extends AbstractStrategy
 {
     const BLOCK_NAME = "hello";
+
+    protected $contentRepository;
+
+    public function __construct(ContentRepositoryInterface $contentRepository)
+    {
+        $this->contentRepository = $contentRepository;
+    }
 
     public function support(ReadBlockInterface $block)
     {
@@ -16,8 +24,12 @@ class HelloBlockDisplayStrategy extends AbstractStrategy
 
     public function show(ReadBlockInterface $block)
     {
+        $language = $this->currentSiteManager->getCurrentSiteDefaultLanguage();
+        $contents = $this->contentRepository->findByContentTypeAndCondition($language, 'car');
+
         return $this->render('FormationBundle:Block/Hello:hello_display_block.html.twig', array(
-            'name' => $block->getAttribute('name')
+            'name' => $block->getAttribute('name'),
+            'contents' => $contents
         ));
     }
 
