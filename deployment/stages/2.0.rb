61262a542558eb68439b8266e57ff5731bced353
Amaury Lavieille <amaury.lavieille@businessdecision.com>
2016-10-07 16:36:53 +0200

    update deploy 2.0

--- deployment/stages/2.0.rb
+++ deployment/stages/2.0.rb
@@ -1,6 +1,6 @@
 set :application, 'OpenOrchestraFront'
 
-server 'open_orchestra_front_inte_1-2', roles: %w{web app db env}
+server 'open_orchestra_front_inte_2-0', roles: %w{web app db env}
 set :deploy_to, '/home/wwwroot/openorchestra/front'
 set :update_dir, 'update-vendor-front-inte'
 set :git_project_dir, 'open-orchestra-front-demo'
