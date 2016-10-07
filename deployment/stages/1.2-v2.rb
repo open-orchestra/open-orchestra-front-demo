66137eba07af294a95fef353f20e23d719e30e12
Amaury Lavieille <amaury.lavieille@businessdecision.com>
2016-10-07 16:14:51 +0200

    update deploy 1.2

--- deployment/stages/1.2-v2.rb
+++ deployment/stages/1.2-v2.rb
@@ -4,4 +4,4 @@ server 'open_orchestra_front2_inte_1-2', roles: %w{web app db env}
 set :deploy_to, '/var/www/front-open-orchestra2'
 set :update_dir, 'update-vendor-front-inte2'
 set :git_project_dir, 'open-orchestra-front-demo'
-set :branch, 'master'
+set :branch, '1.2'
