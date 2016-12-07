516b8d51bc173b7033b8951ae6b1fdfb8236455d
Amaury Lavieille <amaury.lavieille@gmail.com>
2016-12-07 12:31:30 +0100

    update functional test (#322)
    * update functional test
    
    * update vendor

--- src/OpenOrchestra/Tests/Functional/ModelBundle/Repository/NodeRepositoryTest.php
+++ src/OpenOrchestra/Tests/Functional/ModelBundle/Repository/NodeRepositoryTest.php
@@ -648,7 +648,7 @@ class NodeRepositoryTest extends AbstractKernelTestCase
      */
     public function testFindTreeNode()
     {
-        $tree = $this->repository->findTreeNode('2', 'fr');
+        $tree = $this->repository->findTreeNode('2', 'fr', '-');
 
         $this->assertCount(3, $tree);
 
@@ -752,5 +752,30 @@ class NodeRepositoryTest extends AbstractKernelTestCase
             'order' => array($configurationOrder, '2', 'fr', 8),
             'filter' => array($configurationFilter, '2', 'fr', 1),
         );
+    }
+
+    /**
+     * Test update order of brothers
+     */
+    public function testUpdateOrderOfBrothers()
+    {
+        $dm = static::$kernel->getContainer()->get('object_manager');
+        $nodeNews = $this->repository->findOneByNodeId('fixture_page_news');
+        $nodeCommunity = $this->repository->findOneByNodeId('fixture_page_community');
+        $nodeContact= $this->repository->findOneByNodeId('fixture_page_contact');
+        $dm->detach($nodeContact);
+        $dm->detach($nodeCommunity);
+        $dm->detach($nodeNews);
+
+        $this->repository->updateOrderOfBrothers($nodeNews->getSiteId(), $nodeNews->getNodeId(), $nodeNews->getOrder(), $nodeNews->getParentId());
+
+        $nodeNewsAfterUpdate = $this->repository->findOneByNodeId('fixture_page_news');
+        $nodeCommunityAfterUpdate = $this->repository->findOneByNodeId('fixture_page_community');
+        $nodeContactAfterUpdate = $this->repository->findOneByNodeId('fixture_page_contact');
+
+        $this->assertSame($nodeNews->getOrder(), $nodeNewsAfterUpdate->getOrder());
+        $this->assertSame($nodeCommunity->getOrder(), $nodeCommunityAfterUpdate->getOrder());
+        $this->assertSame($nodeContact->getOrder() + 1, $nodeContactAfterUpdate->getOrder());
+
     }
 }
