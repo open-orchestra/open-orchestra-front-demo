d536501d27e803878533ed3b0a175d43c4ab8835
Amaury Lavieille <amaury.lavieille@businessdecision.com>
2017-01-12 18:16:50 +0100

    show content in node

--- src/FormationBundle/DisplayBlock/Strategies/HelloStrategy.php
+++ src/FormationBundle/DisplayBlock/Strategies/HelloStrategy.php
@@ -3,7 +3,11 @@
 namespace FormationBundle\DisplayBlock\Strategies;
 
 use OpenOrchestra\DisplayBundle\DisplayBlock\Strategies\AbstractStrategy;
+use OpenOrchestra\DisplayBundle\Exception\ContentNotFoundException;
+use OpenOrchestra\ModelInterface\Model\ContentInterface;
 use OpenOrchestra\ModelInterface\Model\ReadBlockInterface;
+use OpenOrchestra\ModelInterface\Repository\ContentRepositoryInterface;
+use Symfony\Component\HttpFoundation\RequestStack;
 
 /**
  * Class HelloStrategy
@@ -12,6 +16,27 @@ class HelloStrategy extends AbstractStrategy
 {
     const NAME = 'hello';
 
+    /** @var  RequestStack */
+    protected $requestStack;
+
+    /** @var  ContentRepositoryInterface */
+    protected $contentRepository;
+
+    /**
+     * @param RequestStack $requestStack
+     * @param ContentRepositoryInterface $contentRepository
+     */
+    public function __construct(RequestStack $requestStack, ContentRepositoryInterface $contentRepository)
+    {
+        $this->requestStack = $requestStack;
+        $this->contentRepository = $contentRepository;
+    }
+
+    /**
+     * @param ReadBlockInterface $block
+     *
+     * @return bool
+     */
     public function support(ReadBlockInterface $block)
     {
         return self::NAME === $block->getComponent();
@@ -24,8 +49,13 @@ class HelloStrategy extends AbstractStrategy
      */
     public function show(ReadBlockInterface $block)
     {
+        $contentId = $this->requestStack->getCurrentRequest()->get('contentId');
+        $content = $this->contentRepository->findOneByContentId($contentId);
+        if (!$content instanceof ContentInterface) {
+            throw new ContentNotFoundException();
+        }
         return $this->render('FormationBundle:Block\hello:show.html.twig', array(
-            'name' => $block->getAttribute('name'),
+            'content' => $content
         ));
     }
 
