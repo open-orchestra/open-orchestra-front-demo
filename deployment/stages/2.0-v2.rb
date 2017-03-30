79521d115b6173a5fc3c7b53e250f6401ee4397d
Amaury Lavieille <amaury.lavieille@businessdecision.com>
2017-03-30 16:26:03 +0200

    clean capistrano deploy

--- deployment/stages/2.0-v2.rb
+++
@@ -1,7 +0,0 @@
-set :application, 'OpenOrchestraFront2'
-
-server 'open_orchestra_front2_inte_2-0', roles: %w{web app db env}
-set :deploy_to, '/var/www/front-open-orchestra2'
-set :update_dir, 'update-vendor-front-inte2'
-set :git_project_dir, 'open-orchestra-front-demo'
-set :branch, 'master'
