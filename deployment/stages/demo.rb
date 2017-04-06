0f47eacb43331c7ba7f17acfe03a1e3d09cf689d
Amaury Lavieille <amaury.lavieille@gmail.com>
2017-04-06 11:06:46 +0200

    move conf deploy capistrano (#353)

--- deployment/stages/demo.rb
+++
@@ -1,5 +0,0 @@
-set :application, 'OpenOrchestraFront'
-
-server 'open_orchestra_front_inte_demo', roles: %w{web app db env}
-set :deploy_to, '/home/wwwroot/openorchestra/front'
-set :branch, 'master'
